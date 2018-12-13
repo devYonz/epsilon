12814c9e9fb77be1a0e6e328a3939568f4a2c283
Yonatan Feleke <FounderYonz@gmail.com>
2018-12-13 01:57:15 -0800

    [FinalReport] Rerunning search on DNNs
    The question is to see if a better architecture comes out when we use large sets
    Work so far has focused on using expium to model architecture at 5,000 values
    Minor code cleanup

--- epsilon/src/run.py
+++ epsilon/src/run.py
@@ -203,62 +203,6 @@ def main():
     pass
 
 
-@main.command('logistic')
-@click.option('--data', default='./datasets/Jumble-for-JIRA.json', help='The path to the dataset.')
-@click.option('--spam', default=None, help='The path to the dataset.')
-def logistic(data, spam):
-    """ Test homework binary logistic classifier from HW
-    :param data:
-    :param spam:
-    :return:
-    """
-
-    log.debug(f'Data set file recieved: {data}')
-    with open(data) as f:
-        dataset = json.load(f)
-
-    issue_features = Feature()
-    # TODO: capture the assignee class
-
-    # Build the train matrix
-    issues = []
-    if PROJECT_KEY in dataset:
-        for project in dataset.get(PROJECT_KEY):
-            issues.extend(project.get(ISSUES_KEY))
-    else:
-        issues = dataset
-
-    issues_lines = []
-    assignees = []
-    for issue in issues:
-        words = get_text_body(issue)
-        issues_lines.append(words)
-        assignees.append(issue.get(ASSIGNEE_KEY))
-    # Construct the words into vector of >5 occurance
-    dictionary = create_dictionary(issues_lines)
-    train_matrix = transform_text(issues_lines, dictionary)
-
-    # Y label assignees -> int class number
-    for class_name in list(set(assignees)):
-        issue_features.set_target_id(class_name)
-    assignee_vector = []
-    for assignee in assignees:
-        assignee_vector.append(issue_features.get_target_id(assignee))
-
-    # Build the train feature vector
-    issue_features.data = train_matrix
-    issue_features.target = assignee_vector
-
-    # classify and get output
-    logistic = LogisticClassifier()
-    logistic.fit(issue_features.data, issue_features.target)
-    # test on a training set value
-    x, y = issue_features.get_random_pairs(10)
-    y_predict = logistic.predict(x)
-    mse = 1 / len(y) * np.linalg.norm(y_predict - y, ord=2) ** 2
-    print(f'After training we recieved a MSE: {mse}')
-
-
 @main.command('search')
 @click.option('--data', default='./datasets/Jumble-for-JIRA.json', help='The path to the dataset.')
 @click.option('--dir', 'directory', required=True, help='The path to the dataset.')
@@ -279,9 +223,9 @@ def search(data, prefix, directory):
         f.close()
 
     train_matrix, train_labels = vectorize_json(master_list, directory)
-    neural_net_classifiers = [SVMClassifier(), NBClassifier()]
-    neural_net_classifiers.extend(build_neural_classifier(2000, prefix))
-    # neural_net_classifiers = build_neural_classifier(2000, prefix)
+    # neural_net_classifiers = [SVMClassifier(), NBClassifier()]
+    # neural_net_classifiers.extend(build_neural_classifier(2000, prefix))
+    neural_net_classifiers = build_neural_classifier(2000, prefix)
 
     for classifier in neural_net_classifiers:
         print(" ======== ", classifier.__class__.__name__)
